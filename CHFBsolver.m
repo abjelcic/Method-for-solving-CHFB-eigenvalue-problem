17875bb6dc5c47b34511c6a6eec65eb745a20dae
mavlaho <abjelcic@phy.hr>
2021-02-28 00:11:19 +0100

    final

---
+++ CHFBsolver.m
@@ -0,0 +1,157 @@
+classdef CHFBsolver
+    % CHFBsolver
+    %
+    % Class how to use:
+    % 1.) Instantiate a class with given matrices h,Delta and degeneration
+    % tolerance eps_deg using obj = CHFBsolver( h , Delta , eps_deg ).
+    %
+    % 2.) Once instantiated, the method obj.lambdaIteration( lambda )
+    % returns the function value f(lambda) for given lambda.
+    
+    properties( Access = private )
+        h;
+        Delta;
+        X; % X = (h+1j*Delta)*(h-1j*Delta)
+        eps_deg;
+    end
+    
+    methods( Access = public )
+    
+        function obj = CHFBsolver( h , Delta , eps_deg )
+            % CHFBsolver( h , Delta , eps_deg ) Construct an instance of this class.
+            % h and Delta must be real symmetric nxn matrices.
+            % eps_deg must be real positive number.
+            
+            assert( size(h,1) == size(h,2)      , 'h must be square matrix'         );
+            assert( issymmetric(h) && isreal(h) , 'h must be real symmetric matrix' );
+            
+            assert( size(Delta,1) == size(Delta,2)      , 'Delta must be square matrix'         );
+            assert( issymmetric(Delta) && isreal(Delta) , 'Delta must be real symmetric matrix' );
+            
+            assert( size(h,1) == size(Delta,1) , 'h and Delta must be matrices of the same order' );
+            
+            obj.h     = h;
+            obj.Delta = Delta;
+            
+            % step 0.)
+            X = h + 1j*Delta;
+            X = X * X';
+            X = 0.5 * ( X + X' );
+            obj.X = X;
+            
+            assert( isreal(eps_deg) && size(eps_deg,1)==1 && size(eps_deg,2)==1 && eps_deg>0 , 'eps_deg must be positive real number' );
+            obj.eps_deg = eps_deg;
+            
+            
+            return;
+        end
+        
+        function f = lambdaIteration( obj , lambda )
+            % f = obj.lambdaIteration( lambda ) returns f(lambda) for given
+            % lambda during the lambda iterations for instantianted object
+            % obj with parameters h, Delta and eps_deg.
+            
+            
+            n = size(obj.h,1);
+            
+            h_lambda = obj.h - lambda*eye(n,n);
+            
+            % step 1.)
+            hdhd_lambda = obj.X - 2*lambda*obj.h + (lambda^2)*eye(n,n);
+            
+            % step 2.)
+            [Q,E] = eig( hdhd_lambda );
+            [Q,E] = obj.sortem( Q , E );
+            E  = diag(E);
+            Q1 = real(Q);
+            Q2 = imag(Q);
+            
+            hQ1     = h_lambda  * Q1;
+            hQ2     = h_lambda  * Q2;
+            DeltaQ1 = obj.Delta * Q1;
+            DeltaQ2 = obj.Delta * Q2;
+
+            
+            i = 1;
+            while( i <= n )
+                ev = E(i);
+                
+                % step 3.)
+                j = i;
+                while( j<=n && abs(ev-E(j))<obj.eps_deg )
+                    j = j + 1;
+                end
+                j = j - 1;
+                % E(i) = E(i+1) = ... = E(j-1) = E(j)
+
+
+                % step 4.)
+                e = + Q1(:,i:j)'*hQ1(:,i:j)     - Q2(:,i:j)'*hQ2(:,i:j) ...
+                    + Q1(:,i:j)'*DeltaQ2(:,i:j) + Q2(:,i:j)'*DeltaQ1(:,i:j);
+
+                d = + Q1(:,i:j)'*DeltaQ1(:,i:j) - Q2(:,i:j)'*DeltaQ2(:,i:j) ...
+                    - Q1(:,i:j)'*hQ2(:,i:j)     - Q2(:,i:j)'*hQ1(:,i:j);
+
+                e = 0.5 * ( e + e' );
+                d = 0.5 * ( d + d' );
+
+
+                % step 5.)
+                [qq,ee] = eig( [ e , d ; d , -e ] );
+                [qq,ee] = CHFBsolver.sortem( qq , ee );
+
+                nn = j-i+1;
+                C = qq(    1 : nn    , 1 : nn );
+                S = qq( nn+1 : nn+nn , 1 : nn );
+                
+                
+                % step 6.)
+                Q2(:,i:j) = Q2(:,i:j)*C + Q1(:,i:j)*S;
+
+                
+                i = j + 1;    
+            end
+
+            
+            % step 7.)
+            f = norm( Q2 , 'fro' )^2;
+            return;
+        
+        end
+
+    end
+
+    methods( Static )
+    
+        function [ P2 , D2 ] = sortem( P , D )
+            D2      = diag( sort( diag(D) , 'descend' ) );
+            [~,ind] = sort( diag(D) , 'descend' );
+            P2 = P(:,ind);
+            return;
+        end
+        
+    end
+    
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Test2.m
+++ Test2.m
@@ -1,202 +1,97 @@
 clear;
 close all;
+clc;
 
-
-%ova linija br. threadova na optimalno
+% number of used threads
 maxNumCompThreads('automatic'); 
 
+eps_deg  = (1.e-3)^2; % eigenvalue degeneration tolerance in [MeV^2]
+N_lambda = 5;         % number of artificial lambda iteration
 
-
-degentol = 1.e-6; 
-N_lambda = 5; %number of artificial lambda iteration
-
-%Hamiltonian with positive spectrum given below will be constructed
+% Generation of degenerated symmetric Hamiltonian matrix
 nd = 1000;
 Spectrum = [ -1*ones(1,nd) , ...
              -2*ones(1,nd) , ...
-              3*ones(1,nd) ];
+             +3*ones(1,nd) ]; % [MeV]         
+n     = length( Spectrum );
+h     = randSymmetricMatrix( Spectrum );
+Delta = 1.e-12 * ones(n,n);
 
-                
-
-
-
-
-
-
-
-Lambdas = randn(1,N_lambda);
-
-n = length(Spectrum);
-h = RandSymmetricMatrix( Spectrum );
-Delta = 1.e-12*ones(n,n);
-H = [ h , Delta ; Delta , -h ];
+% lambdas are generated to simulate the root-finding in lambda iterations
+lambdas = 10 * randn( 1 , N_lambda ); % [MeV]
 
 
 
 
 
-tic
-%% Moja metoda
 
-hd   = h + 1j*Delta;
-hdhd = hd * hd';
-hdhd = 0.5*( hdhd + hdhd' );
+% proposed method
+tic;
+TargetValues1 = zeros( 1 , N_lambda );
 
-TargetValues1 = [];
-for lambda = Lambdas
-    
-    h_lambda = h - lambda*eye(n,n);
-    hdhd_lambda = hdhd - 2*lambda*h + (lambda^2)*eye(n,n);
-    
-    [Q,D] = eig(hdhd_lambda);
-    [Q,D] = sortem(Q,D);
-    D = diag(D);
-    Q1 = real(Q);
-    Q2 = imag(Q);
-
-    % Odkomentiraj ovo i uvjeri se da ed matrica ima blokdiag formu za
-    % degenerirani slucaj
-    % ed = [Q1,-Q2;Q2,Q1]' * [h,Delta;Delta,-h] * [Q1,-Q2;Q2,Q1];
-    % surf(log10(abs(ed)))
-    % return;
-
-    hQ1     = h_lambda*Q1;
-    hQ2     = h_lambda*Q2;
-    DeltaQ1 = Delta*Q1;
-    DeltaQ2 = Delta*Q2;
-
-
-    i = 1;
-    while i <= n
-        ev = D(i);
-
-        j = i;
-        while j<=n && abs(ev-D(j))<degentol
-            j = j + 1;
-        end
-        j = j - 1;
-
-        % sad su od i-te do j-te (uljkucivo) sv. vrijednosti degenerirane!
-
-
-
-        % tu bi se mozda dalo nesto ustedi operacija, ali za sad ne brinem
-        e = + Q1(:,i:j)'*hQ1(:,i:j)     - Q2(:,i:j)'*hQ2(:,i:j) ...
-            + Q1(:,i:j)'*DeltaQ2(:,i:j) + Q2(:,i:j)'*DeltaQ1(:,i:j);
-
-        d = + Q1(:,i:j)'*DeltaQ1(:,i:j) - Q2(:,i:j)'*DeltaQ2(:,i:j) ...
-            - Q1(:,i:j)'*hQ2(:,i:j)     - Q2(:,i:j)'*hQ1(:,i:j);
-
-        e = 0.5*(e+e');
-        d = 0.5*(d+d');
-
-
-
-        [qq,ee] = eig( [ e , d ; d , -e ] );
-        [qq,ee] = sortem(qq,ee);
-
-        % qq ima oblik qq=[C,-S;S,C], gdje su u slucaju nn=1 stvarno
-        % C i S obicni brojevi cos i sin
-        nn = j-i+1;
-        C = qq(1:nn,1:nn);
-        S = qq(nn+1:nn+nn,1:nn);
-
-        Q2(:,i:j) = Q2(:,i:j)*C + Q1(:,i:j)*S;
-
-        i = j + 1;    
-    end
-
-    f1 = norm( Q2 , 'fro' )^2;
-    TargetValues1 = [ TargetValues1 ; f1 ];
+chfbsolver = CHFBsolver( h , Delta , eps_deg );
+for i = 1 : N_lambda
+    TargetValues1(i) = chfbsolver.lambdaIteration( lambdas(i) );
 end
-%%
 t1 = toc;
 
 
 
+
+
+
+% straightforward method
 tic;
-%% Naivna metoda
-TargetValues2 = [];
-for lambda = Lambdas
-    [Q,E] = eig( H - lambda*[eye(n),zeros(n);zeros(n),-eye(n)] );
-    [Q,E] = sortem(Q,E);
-    f2 = norm( Q( n+1:end , 1:n ) , 'fro' )^2; 
-    TargetValues2 = [ TargetValues2 ; f2 ];
+TargetValues2 = zeros( 1 , N_lambda );
+
+for i = 1 : N_lambda
+    
+    [Q,E] = eig( [h,Delta;Delta,-h] - lambdas(i)*[eye(n),zeros(n);zeros(n),-eye(n)] );
+    [Q,E] = CHFBsolver.sortem( Q , E );
+     
+    TargetValues2(i) = norm( Q( n+1:end , 1:n ) , 'fro' )^2;
 end
-%%
 t2 = toc;
 
 
 
 
 
+
+
+
+% short report
 thr = maxNumCompThreads;
-fprintf('Koristim %d threadova procesora.\n', thr);
-fprintf('Velicina problema n=%d, dakle %dx%d Hamiltonijan je u pitanju!\n\n', n, 2*n, 2*n );
-fprintf('Napravljene su %d lambda iteracije.\n', N_lambda);
-fprintf('Moja   metoda: %f sec\n', t1   );
-fprintf('Naivna metoda: %f sec\n', t2   );
-fprintf('Moj je %f puta brzi! \n', t2/t1);
+fprintf('Used number of threads:      %d.\n' , thr );
+fprintf('Number of lambda iterations: %d.\n' , N_lambda );
+fprintf('Problem size: n = %d, i.e. H is %dx%d symmetric Hamiltonian matrix.\n' , n , 2*n , 2*n );
 fprintf('\n');
+fprintf('Time elapsed (proposed method):        %f s.\n' , t1 );
+fprintf('Time elapsed (straightforward method): %f s.\n' , t2 );
+fprintf('Time elapsed ratio:                    %f.\n'   , t2/t1 );
 fprintf('\n');
-fprintf('Usporedba target values sa obje metode: \n');
-[ TargetValues1 , TargetValues2 , TargetValues1-TargetValues2 ]
+fprintf('Comparison of target values: \n');
+for i = 1 : N_lambda
+    lambda = lambdas(i);
+    f1     = TargetValues1(i);
+    f2     = TargetValues2(i);
+    diff   = abs(f1-f2);
+    fprintf('lambda = %10.5f MeV, f_1 = %10.5f, f_2 = %10.5f., |f1-f2| = %10.5e\n' , lambda , f1 , f2 , diff );
+end
 
 
 
 
 
 
-function A = RandSymmetricMatrix( Spectrum )
-    Q = RandOrhogonalMatrix( length(Spectrum) );
-    A = Q'*diag(Spectrum)*Q;
-    A = 0.5*(A+A');
+
+
+
+
+function A = randSymmetricMatrix( Spectrum )
+    n     = length(Spectrum);
+    [Q,~] = qr( randn(n,n) );
+    A     = Q' * diag(Spectrum) * Q;
+    A     = 0.5 * ( A + transp(A) );
     return;
 end
-
-function Q = RandOrhogonalMatrix( n )
-    [Q,~,~] = svd( randn(n,n) );
-    return;
-end
-
-function [ P2 , D2 ]=sortem( P , D )
-    D2 = diag(sort(diag(D),'descend'));
-    [~,ind] = sort(diag(D),'descend');
-    P2 = P(:,ind);
-end
-
-function H = RandHamiltonianMatix( PositiveSpectrum )
-    
-    n = length(PositiveSpectrum);
-    [U,~,~] = svd(randn(n,n));
-    [V,~,~] = svd(randn(n,n));
-    
-    C = sqrt( rand(n,1) );
-    S = sqrt( ones(n,1) - C.^2 );
-    C = diag(C);
-    S = diag(S);
-
-    Q1 = U*C*V';
-    Q2 = U*S*V';
-    
-    H = [Q1,-Q2;Q2,Q1]' * [ diag(PositiveSpectrum) , zeros(n,n) ; ...
-                            zeros(n,n)             , -diag(PositiveSpectrum)  ] * ...
-        [Q1,-Q2;Q2,Q1];
-    
-    H = 0.5*(H+H');
-    
-    return;
-end
-
-function A = randsymm( n )
-    A = zeros(n,n);
-    for i = 1 : n
-        for j = i+1 : n
-            A(i,j) = randn();
-            A(j,i) = A(i,j);
-        end
-        A(i,i) = randn();
-    end
-    return;
-end
-
